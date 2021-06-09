ebdf7773fb3d3fac67a8fbec0373baf7fb425213
TonyIbombot <71134792+TonyIbombot@users.noreply.github.com>
2021-06-09 09:15:18 +0200

    Add files via upload

---
+++ AjoutSalon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PreambulePPE
+{
+    public partial class AjoutSalon : Form
+    {
+        public AjoutSalon()
+        {
+            InitializeComponent();
+        }
+
+        private void BT_Ajout_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                Salon unSalon = new Salon(TB_Nom.Text, TB_Lieu.Text,DTP_debut.Value, DTP_Fin.Value);
+                unSalon.Save();
+                this.DialogResult = DialogResult.OK;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
